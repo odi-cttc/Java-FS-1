cf4b033104abfb2a99b63fc259188024510017ad
CTTC <CTTC@DESKTOP-PSFK2SM>
2024-11-28 09:15:01 +0530

    18/11

--- crud-api-demo/src/main/java/com/cttc/emp/EmployeeController.java
+++ crud-api-demo/src/main/java/com/cttc/emp/EmployeeController.java
@@ -3,6 +3,7 @@ package com.cttc.emp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cttc.entity.Employee;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/emp")
 public class EmployeeController {
