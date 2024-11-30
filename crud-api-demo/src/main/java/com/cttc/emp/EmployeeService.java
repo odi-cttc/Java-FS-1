1323b3d5d2bde062a9becfed84a6d1775b2a061f
CTTC <CTTC@DESKTOP-PSFK2SM>
2024-11-30 07:41:16 +0530

    29/11

--- crud-api-demo/src/main/java/com/cttc/emp/EmployeeService.java
+++ crud-api-demo/src/main/java/com/cttc/emp/EmployeeService.java
@@ -13,4 +13,6 @@ public interface EmployeeService {
 
 	List<Employee> getAllEmp();
 
+	Employee findById(Long id);
+
 }
--- crud-api-demo/src/main/java/com/cttc/emp/EmployeeServiceImpl.java
+++ crud-api-demo/src/main/java/com/cttc/emp/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cttc.emp;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 import com.cttc.entity.Employee;
 
 @Service
-public class EmployeeServiceImpl implements EmployeeService{
+public class EmployeeServiceImpl implements EmployeeService {
 
 	@Autowired
 	EmployeeDao dao;
-	
+
 	@Override
 	public Employee saveEmployee(Employee employee) {
 		return dao.save(employee);
@@ -23,4 +24,11 @@ public class EmployeeServiceImpl implements EmployeeService{
 		return (List<Employee>) dao.findAll();
 	}
 
+	@Override
+	public Employee findById(Long id) {
+		Optional<Employee> empById = dao.findById(id);
+		if (empById.isPresent())
+			return empById.get();
+		return null;
+	}
 }
